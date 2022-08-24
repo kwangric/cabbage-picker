695ffa43535d20bd902b5487eb46259999dceecf
kwangric <43795498+kwangric@users.noreply.github.com>
2022-08-24 16:36:44 -0400

    Add detail to generator card

--- components/CabbageCounter.js
+++ components/CabbageCounter.js
@@ -11,7 +11,7 @@ const CabbageCounter = () => {
   return (
     <Box className={cabbageCounterStyles.counter}>
       <Typography variant="h5">Cabbages: {Math.floor(cabbages)}</Typography>
-      <Typography variant="subtitle1">CpS: {cabbagesPerSecond % 1 === 0 ? cabbagesPerSecond.toFixed(0) : cabbagesPerSecond.toFixed(1)}</Typography>
+      <Typography variant="subtitle1">{`(${cabbagesPerSecond % 1 === 0 ? cabbagesPerSecond.toFixed(0) : cabbagesPerSecond.toFixed(1)} per second)`}</Typography>
     </Box>
   )
 }
--- components/GeneratorList.js
+++ components/GeneratorList.js
@@ -11,75 +11,28 @@ import Tooltip, { tooltipClasses } from '@mui/material/Tooltip'
 const GeneratorList = () => {
   const { generators, buyGenerator } = useContext(GameContext)
 
-  const HtmlTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#f5f5f9',
-      color: 'rgba(0, 0, 0, 0.87)',
-      maxWidth: 220,
-      fontSize: theme.typography.pxToRem(12),
-      border: '1px solid #dadde9',
-    },
-  }))
-
   return (
-    <div className={generators.filter((generator) => generator.visible === true).length > 0 ? `${generatorListStyles.container}` : `${generatorListStyles.container} ${generatorListStyles.hidden}`}>
+    <div
+      className={
+        generators.filter((generator) => generator.visible === true).length > 0
+          ? `${generatorListStyles.container}`
+          : `${generatorListStyles.container} ${generatorListStyles.hidden}`
+      }
+    >
       {generators.map((generator) => {
         if (generator.visible) {
           return (
             <div>
-              <HtmlTooltip
-                title={
-                  <>
-                    <Typography color="inherit">{generator.name}</Typography>
-                    <Typography variant="caption">
-                      {`Each ${generator.name} produces ${
-                        generator.id === 1
-                          ? generator.baseIncome.toFixed(1)
-                          : generator.baseIncome
-                      } cabbages per second.`}
-                    </Typography>
-                    {generator.quantity > 0 ? (
-                      <>
-                        <br />
-                        <Typography variant="caption">
-                          {`${generator.quantity} ${
-                            generator.name
-                          }s producing ${
-                            generator.id === 1 &&
-                            (generator.quantity * generator.baseIncome) % 1 !==
-                              0
-                              ? (
-                                  generator.quantity * generator.baseIncome
-                                ).toFixed(1)
-                              : (
-                                  generator.quantity * generator.baseIncome
-                                ).toFixed(0)
-                          } cabbages per second.`}
-                        </Typography>
-                      </>
-                    ) : (
-                      <></>
-                    )}
-                    <br />
-                    <Typography
-                      variant="caption"
-                      className="generatorListStyles.description"
-                    >
-                      {`"${generator.description}"`}
-                    </Typography>
-                  </>
-                }
-                placement="left"
+              <div
+                className={generatorListStyles.generatorCard}
+                key={generator.id}
               >
-                <div
-                  className={generatorListStyles.generatorCard}
-                  key={generator.id}
-                >
-                  <div className={generatorListStyles.cardInfo}>
+                <div className={generatorListStyles.cardInfo}>
+                  <div className={generatorListStyles.cardTop}>
                     <div className={generatorListStyles.cardLeft}>
-                      <Typography variant="body1" sx={{marginBottom: '5px'}}>{generator.name}</Typography>
+                      <Typography variant="body1" sx={{ marginBottom: '5px' }}>
+                        {generator.name}
+                      </Typography>
                       <div className={generatorListStyles.cost}>
                         <div className={generatorListStyles.cabbageContainer}>
                           <Image
@@ -98,23 +51,58 @@ const GeneratorList = () => {
                       </div>
                     </div>
                     <div className={generatorListStyles.cardRight}>
-                      <Typography variant="body2">{`Quantity: ${generator.quantity}`}</Typography>
-                      <br />
-                      <div className={generatorListStyles.buttonContainer}>
-                    <Button
-                      size="small"
-                      variant="contained"
-                      className={generatorListStyles.buyButton}
-                      onClick={() => buyGenerator(generator.id)}
-                    >
-                      Buy
-                    </Button>
-                  </div>
+                      <Typography variant="body1">{`Quantity: ${generator.quantity}`}</Typography>
                     </div>
                   </div>
-
+                  <div className={generatorListStyles.cardBottom}>
+                    <div className={generatorListStyles.cardLeft}>
+                      <Typography
+                        className={generatorListStyles.cps}
+                        variant="caption"
+                      >
+                        {`Each: ${
+                          generator.id === 1
+                            ? generator.baseIncome.toFixed(1)
+                            : generator.baseIncome
+                        } CpS`}
+                      </Typography>
+                      <Typography
+                        className={generatorListStyles.cps}
+                        variant="caption"
+                      >
+                        {`Total: ${
+                          generator.id === 1 &&
+                          (generator.quantity * generator.baseIncome) % 1 !== 0
+                            ? (
+                                generator.quantity * generator.baseIncome
+                              ).toFixed(1)
+                            : (
+                                generator.quantity * generator.baseIncome
+                              ).toFixed(0)
+                        } CpS`}
+                      </Typography>
+                    </div>
+                    <div className={generatorListStyles.cardRight}>
+                      <div className={generatorListStyles.buttonContainer}>
+                        <Button
+                          size="small"
+                          variant="contained"
+                          className={generatorListStyles.buyButton}
+                          onClick={() => buyGenerator(generator.id)}
+                        >
+                          Buy
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                  <Typography
+                      variant="caption"
+                      sx={{fontStyle: "italic"}}
+                    >
+                      {`"${generator.description}"`}
+                    </Typography>
                 </div>
-              </HtmlTooltip>
+              </div>
             </div>
           )
         }
