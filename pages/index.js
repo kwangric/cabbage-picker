2f001c61342a7769ff4048cdb8beab7e3456e98e
kwangric <43795498+kwangric@users.noreply.github.com>
2022-08-25 13:16:12 -0400

    Add cabbage total to site title

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Game from '../components/Game'
 import Head from 'next/head'
+import { useContext } from 'react'
+import { GameContext } from '../context/GameContext'
 import styles from '../styles/Game.module.css'
 
 export default function Home() {
+  const { cabbages } = useContext(GameContext)
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Cabbage Picker</title>
+        <title>{`${Math.floor(cabbages)} cabbages | Cabbage Picker`}</title>
         <link rel="icon" href="/favicon.ico" />
 
         <meta property="og:title" content="Cabbage Picker" />
